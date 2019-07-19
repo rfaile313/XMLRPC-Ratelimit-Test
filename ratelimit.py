a1c490dae51ae632ecda1daecb8cabedb66aef18
Rudy Faile <febesbrasil@gmail.com>
2019-07-19 00:00:56 -0500

    cleaned up methods, added more catches to input

--- ratelimit.py
+++ ratelimit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""ratelimit.py: For rate testing a site's XMLRPC file against the host."""
+"""ratelimit.py: For rate testing a site's XML-RPC file against the host."""
 __author__ = "@rfaile313"
 
 import requests
 import time
 import os
 
-TIMES_TO_TEST = 49 #50 bc index starts @ 0
+TIMES_TO_TEST = 50 
 TIMED_OUT = "Request took longer than a minute to complete. Timed out."
 RATELIMIT = "Ratelimit test for:"
 
@@ -20,24 +20,24 @@ data = {
 }
 
 def detect_https(url):
-    if "http" not in url:
+    if "http" not in url and '/xmlrpc.php' not in url:
+        return "https://{}/xmlrpc.php".format(url)
+    elif "/xmlrpc.php" in url and "http" not in url:
         return "https://{}".format(url)
     else:
-        return url
+        return url + '/xmlrpc.php'
   
-URL = detect_https(input("Enter the URL. Don't include XMLRPC.php - example: https://example.com \nURL:"))
+URL = detect_https(input("Enter the URL. Exmaple: https://example.com \nURL:"))
 
-def test_rate(times_to_run, url):
-    current_iteration = 0
+def test_rate(url):
     start_time = time.time()
     results = []
     print(RATELIMIT, url)
-    while current_iteration <= times_to_run:
-        response = requests.post('{}/xmlrpc.php'.format(url), headers=headers, data=data)
+    for i in range(TIMES_TO_TEST):
+        response = requests.post(url, headers=headers, data=data)
         string_response = response_codes(response)
         print(string_response)
         results.append(string_response)
-        current_iteration += 1
         ### This section checks to see if its been longer than a minute, then times out. 
         current_time = time.time()
         check_time = str(round((current_time - start_time), 2))
@@ -48,7 +48,7 @@ def test_rate(times_to_run, url):
         ### End timeout check
     end_time = time.time()
     total_time = str(round((end_time - start_time), 2))
-    ending_information = "Done. {} Requests made. Time elapsed: {} Seconds.".format(current_iteration, (total_time))
+    ending_information = "Done. {} Requests made. Time elapsed: {} Seconds.".format(i + 1, (total_time))
     print (ending_information)
 
     ## Ask/Write to file
@@ -75,7 +75,8 @@ def ask_yes_no(question):
     return response
 
 def response_codes(response):
-    #turn response into string
+    #Common Response Codes
+    #TODO could definitely add more, or be more specific 🤷
     string_response = str(response)
     if "20" in string_response:
         string_response = string_response + " OK"
@@ -93,4 +94,4 @@ def response_codes(response):
         string_response = string_response + " Look up Response Code"
     return string_response
 
-test_rate(TIMES_TO_TEST, URL)
+test_rate(URL)
